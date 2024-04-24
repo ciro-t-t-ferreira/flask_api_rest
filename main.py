25b5bc0d9a86670cc592ffa8fd4a169294f5cf53
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-04-24 14:36:09 -0300

    create get and put for videos

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
+
+#this is similar to moongose models
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name" , type = str, help="Name of the Video is required" , required = True)
+video_put_args.add_argument("views", type = int, help="Views of the Video is required", required = True)
+video_put_args.add_argument("likes", type = int, help="Likes of the Video is required", required = True)
+
+videos = {}
+
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message = "Video id is not valid")
 
 #setting a endpoint with flask:
 #create classe that inherit Resource
 #That allow to override request methods such as get
 #add the classe to the api with add_resource
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"Hello World"}
-
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True) #set debug=True only for development enviroment
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + 'helloworld')
+response = requests.put(BASE + 'video/1', {"likes":10, "name": "Flask is fun", "views": 137137})
+print(response.json())
+input() #that will wait for a input of the user
+response = requests.get(BASE + 'video/5')
 print(response.json())
